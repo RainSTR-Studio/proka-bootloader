82318637a82853e63b563e300349448f51e65b1e
zhangxuan2011 <zx20110412@outlook.com>
2026-06-27 08:43:09 +0800

    feat: Added commit message

--- legacy/drivers/iso9660.asm
+++ legacy/drivers/iso9660.asm
@@ -4,4 +4,25 @@
 ; This file is the ISO9660 reader.
 [bits 16]	; Still real mode :/
 
+; ==============================
+; load_iso9660_file
+; Load one file from ISO9660 filesystem.
+; In: DS:SI - The filename
+;     ES:BX - The load position
+; ==============================   
+load_iso9660_file:
+  pushad
+  push es
+  mov bp, sp
+  cld
 
+  ; Save ES, BX
+  mov [save_es], es
+  mov [save_bx], bx
+
+
+; =======================================
+; DATA SECTIONS
+; =======================================
+save_es dw 0
+save_bx dw 0
